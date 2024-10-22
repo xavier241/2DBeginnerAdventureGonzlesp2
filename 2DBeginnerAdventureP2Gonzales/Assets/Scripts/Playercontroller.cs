0e1a1f446e6ad82a8ac5f4cb53e56d3ed0e7587f
xavier241 <Xaviergonzales009@gmail.com>
2024-10-22 09:57:14 -0700

    Added movement to ducko
    Added movement and amlost created the tiles for ducko

---
+++ 2DBeginnerAdventureP2Gonzales/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log(horizontal);
+
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        transform.position = position;
+    }
+}
