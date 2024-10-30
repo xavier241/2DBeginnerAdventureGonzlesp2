4bcf728d5fccc3622f20474de9161f12c449eaf0
xavier241 <Xaviergonzales009@gmail.com>
2024-10-30 09:58:25 -0700

    Added rigidbodys and box colliders
    Added rigidbodys and box colliderds
    almosted done with blocking movementts

--- 2DBeginnerAdventureP2Gonzales/Assets/Scripts/Playercontroller.cs
+++ 2DBeginnerAdventureP2Gonzales/Assets/Scripts/Playercontroller.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    Rigidbody2D Rigidbody2d;
+    float horizontal;
+    float vertical; 
     public float speed = 4.0f;
     // Start is called before the first frame update
     void Start()
     {
+        Rigidbody2d = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
         Debug.Log(horizontal);
 
-        Vector2 position = transform.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        Vector2 position = Rigidbody2d.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         transform.position = position;
+        Rigidbody2d.MovePosition(position);
     }
+  
 }
